f543f9fe1aadad4981562ee92dd6c79dc03434e4
Tymur Prokopiev <tproko@softserveinc.com>
2024-01-06 19:03:09 +0200

    added first method

--- Runtime/SampleMethods.cs
+++ Runtime/SampleMethods.cs
@@ -1,7 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-namespace MyTools.Tools{
-    public class SampleMethods : MonoBehaviour{
-        
+namespace MyTools.Tools
+{
+
+    public class SampleMethods : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log(Random.value);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
     }
 }
