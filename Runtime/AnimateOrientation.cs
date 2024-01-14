b304fb9c4bc7213a53489dd3b8f714fff1c86df8
Tymur Prokopiev <tproko@softserveinc.com>
2024-01-14 15:27:47 +0200

    regular update

--- Runtime/AnimateOrientation.cs
+++ Runtime/AnimateOrientation.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class AnimateOrientation : MonoBehaviour
 {
     private Transform MyTransform = null;
-    [SerializeField] Vector3 rotation = new Vector3(0f, 0f, 0f);
+    [SerializeField] Vector3 rotation = new Vector3(0f,0f,0f);
     [SerializeField, Range(0.0f, 100.0f)] float speed = 0;
     [SerializeField] bool randomize = false;
     [SerializeField] bool setSeed = false;
@@ -11,41 +11,34 @@ public class AnimateOrientation : MonoBehaviour
     int initialSeed;
     float initialRandom;
     float random;
+    Random.State stateOnStart;
 
     void Start()
     {
         MyTransform = GetComponent<Transform>();
         initialSeed = (int)Random.Range(0.0f, 9999.0f);
         initialRandom = Random.Range(0.0f, 360.0f);
-
-        // Always initialize with initialSeed on Start
         Random.InitState(initialSeed);
     }
 
     void Update()
     {
-        if (setSeed)
-        {
-            // If setSeed is true, use the provided seed
+        if (setSeed){
             Random.InitState(seed);
             random = Random.Range(0.0f, 360.0f);
         }
-        else
-        {
-            // If setSeed is false, use the initial seed
-            Random.InitState(initialSeed);
-            random = randomize ? initialRandom : 0.0f;
-        }
-
+        
+        Random.InitState(initialSeed);
         float time = Time.time;
         Vector3 currentRotation = rotation * time * speed;
 
-        if (randomize && !setSeed)
-        {
-            // Add initialRandom only when randomize is true and setSeed is false
-            currentRotation += new Vector3(random, random, random);
+        if (randomize){
+            if (setSeed){
+                currentRotation += new Vector3(random, random, random);
+            }
+        currentRotation += new Vector3(initialRandom, initialRandom, initialRandom);
         }
-
+        
         MyTransform.rotation = Quaternion.Euler(currentRotation);
     }
 }
