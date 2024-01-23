211d8af87303c327df2f7c690fa4dd502907af49
Tymur Prokopiev <tproko@softserveinc.com>
2024-01-23 10:50:01 +0200

    regular update

---
+++ Runtime/DeviceSimulatorCanvasScaler.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+[AddComponentMenu("My Tools/Utility/" + nameof(LogController))]
+
+public class DeviceSimulatorCanvasScaler : MonoBehaviour
+{
+    GameObject _xrDeviceSimulator;
+    Canvas _canvas;
+    CanvasScaler _canvasScaler;
+    
+    [SerializeField] bool helpUI = true;
+    [SerializeField, Range(0.4f, 1.0f)] float scaleFactor = 0.5f;
+
+    void Start()
+    {
+        _xrDeviceSimulator = gameObject;
+        _canvas = _xrDeviceSimulator.GetComponentInChildren<Canvas>();
+        _canvasScaler = _xrDeviceSimulator.GetComponentInChildren<CanvasScaler>();
+    }
+
+    void Update()
+    {
+        if (_xrDeviceSimulator != null)
+        {
+            _canvas.enabled = helpUI;
+            _canvas.pixelPerfect = true;
+            _canvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ConstantPixelSize;
+            _canvasScaler.scaleFactor = scaleFactor;
+        }
+        else
+        {
+            Debug.LogError("XR Device Simulator UI(Clone) not found.");
+        }
+    }
+}
