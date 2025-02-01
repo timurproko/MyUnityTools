53ad5ef52646dad841bb37533d6e70c3e83ac92a
pojitonov <pojitonov@gmail.com>
2025-02-01 19:28:41 +0200

    Menus structure uodate

--- Scripts/Editor/Core/Menus.cs
+++ Scripts/Editor/Core/Menus.cs
@@ -7,15 +7,15 @@
         public const int ASSETS_INDEX = 10000;
         public const string ASSETS_MENU = MY_TOOLS_MENU + "Assets/";
         
-        public const int EDITOR_INDEX = 20000;
+        public const int EDITOR_INDEX = 11000;
         public const string EDITOR_MENU = MY_TOOLS_MENU + "Editor/";
         
-        public const int SCENE_VIEW_INDEX = 30000;
-        public const string BOOKMARKS_MENU = MY_TOOLS_MENU + "Scene View Bookmarks/";
+        public const int SCENE_VIEW_INDEX = 12000;
         public const string NAVIGATION_MENU = MY_TOOLS_MENU + "Scene View Navigation/";
+        public const string BOOKMARKS_MENU = MY_TOOLS_MENU + "Scene View Bookmarks/";
         public const string TOOLS_MENU = MY_TOOLS_MENU + "Scene View Tools/";
         
-        public const int SELECTION_INDEX = 40000;
+        public const int SELECTION_INDEX = 13000;
         public const string SELECTION_MENU = MY_TOOLS_MENU + "Selection Groups/";
 
         public const int MODES_INDEX = 99000;
