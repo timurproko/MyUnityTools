5ea288ae0bb313e1b1eb98aa7237a5b4b0ac85d3
pojitonov <pojitonov@gmail.com>
2025-04-23 11:29:05 +0300

    added play modes for Play Animation

--- Scripts/Runtime/Animation/PlayAnimation.cs
+++ Scripts/Runtime/Animation/PlayAnimation.cs
@@ -1,7 +1,9 @@
 #if ANIMANCER
+using System;
 using Animancer;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 [RequireComponent(typeof(Animator))]
 [RequireComponent(typeof(AnimancerComponent))]
@@ -9,39 +11,75 @@ public class PlayAnimation : MonoBehaviour
 {
     [SerializeField] private AnimationClip _Animation;
     [SerializeField] private PlayModes _playMode;
+    [SerializeField] private bool _loop;
     [SerializeField] private bool _randomStartTime;
-    
+
     [ShowIf("_randomStartTime")]
     [SerializeField] private int _randomSeed;
 
     private AnimancerComponent _Animancer;
+    private AnimancerState _State;
 
     private void Awake()
     {
         _Animancer = GetComponent<AnimancerComponent>();
     }
 
-    protected virtual void OnEnable()
+    private void Start()
     {
-        if (_playMode == PlayModes.OnEnable)
+        if (_playMode == PlayModes.OnStart)
         {
-            var state = _Animancer.Play(_Animation);
+            PlayAnimationClip();
+        }
+    }
 
-            if (_randomStartTime)
+    private void OnEnable()
+    {
+        if (_playMode == PlayModes.OnEnable && _Animancer != null)
+        {
+            PlayAnimationClip();
+        }
+
+        if (_State != null)
+            _State.IsPlaying = true;
+    }
+
+    private void OnDisable()
+    {
+        if (_State != null)
+            _State.IsPlaying = false;
+    }
+
+    private void PlayAnimationClip()
+    {
+        _State = _Animancer.Play(_Animation);
+        _State.IsPlaying = enabled;
+
+        if (_randomStartTime)
+        {
+            if (_randomSeed != 0)
+                Random.InitState(_randomSeed);
+
+            _State.NormalizedTime = Random.value;
+        }
+
+        if (_loop)
+        {
+            _State.Events(this).OnEnd = () =>
             {
-                if (_randomSeed != 0) 
-                {
-                    Random.InitState(_randomSeed); 
-                }
-                
-                state.NormalizedTime = Random.value;
-            }
+                _Animancer.Play(_Animation).NormalizedTime = 0;
+            };
+        }
+        else
+        {
+            _State.Events(this).OnEnd = null;
         }
     }
 }
 
 public enum PlayModes
 {
+    OnStart,
     OnEnable
 }
 #endif
